4d679a138e458d104be0d1243b7ecac4356644ab
s3ns3i <eragonsm@gmail.com>
2019-01-29 01:21:44 +0100

    Transmitter working fine

--- transmitter/transmitter.ino
+++ transmitter/transmitter.ino
@@ -2,21 +2,121 @@
 
 #include <RH_ASK.h>
 #include <SPI.h> // Not actually used but needed to compile
+#include <EEPROM.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <string.h>
 
+#define TOO_HOT "HOT"
+#define TOO_COLD "COL"
+#define OK "OK"
+#define TOO_HOT_ADDR 0
+#define TOO_COLD_ADDR 1
+#define THERMOMETER_INDEX 0
+#define TEMP_TOO_HOT 25
+#define TEMP_TOO_COLD 24
+
+const byte ONEWIRE_PIN = 11;
+
+OneWire onewire(ONEWIRE_PIN);
+DallasTemperature sensors(&onewire);
 RH_ASK driver;
 
-const char *msg = "HOT:90.00";
+byte thermometerAddress[8];
+char msg[10] = "";
+bool buttonPressed = false;
 
 void setup()
 {
-    Serial.begin(4800); // Debugging only
+    Serial.begin(9600); // Debugging only
     if (!driver.init())
+    {
         Serial.println("init failed");
+    }
+    findThermometerAddress();
+}
+
+void findThermometerAddress()
+{
+  onewire.reset_search();
+  while(onewire.search(thermometerAddress))
+  {
+    if (thermometerAddress[0] != 0x28)
+      continue;
+
+    if (OneWire::crc8(thermometerAddress, 7) != thermometerAddress[7])
+    {
+      Serial.println(F("Błędny adres, sprawdz polaczenia"));
+      break;
+    }
+
+    for (byte i=0; i<8; i++)
+    {
+      Serial.print(F("0x"));
+      Serial.print(thermometerAddress[i], HEX);
+
+      if (i < 7)
+        Serial.print(F(", "));
+    }
+    Serial.println();
+  }
 }
 
 void loop()
 {
+    // loadData();
+    readTemperature();
+    interpretTemperature();
     sendData();
+    delay(1000);
+}
+
+void loadData()
+{
+    Serial.println("loadData() - Not yet implemented.");
+}
+
+uint8_t *loadData(const char *KEY)
+{
+    Serial.println("loadData(const char* KEY) - Not yet implemented.");
+    // return "";
+}
+
+void readTemperature()
+{
+    sensors.requestTemperatures();
+}
+
+void interpretTemperature()
+{
+    msg[0] = '\0';
+    float currentTemperature = sensors.getTempCByIndex(THERMOMETER_INDEX);
+    char tempString[6];
+
+    dtostrf(currentTemperature, 5, 2, tempString);
+    // Serial.print("Current temperature: ");
+    // Serial.println(tempString);
+    if(currentTemperature <= TEMP_TOO_COLD)
+    {
+        strcat(msg, TOO_COLD);
+        msg[3] = ':';
+        msg[4] = '\0';
+    }
+    else if(currentTemperature >= TEMP_TOO_HOT)
+    {
+        strcat(msg, TOO_HOT);
+        msg[3] = ':';
+        msg[4] = '\0';
+    }
+    else
+    {
+        strcat(msg, OK);
+        msg[2] = ':';
+        msg[3] = '\0';
+    }
+    strcat(msg, tempString);
+
+    Serial.println(msg);
 }
 
 void sendData()
@@ -24,7 +124,21 @@ void sendData()
     driver.send((uint8_t *)msg, strlen(msg));
     driver.waitPacketSent();
     Serial.println("Sent package");
-    delay(1000);
+}
+
+void saveData(uint8_t *data)
+{
+    Serial.println("Saving data to EEPROM");
+    uint8_t key[4];
+    uint8_t value[8];
+    //   readData(data, key, value);
+    Serial.print("Int value: ");
+    Serial.println((char *)value);
+    EEPROM.write(TOO_HOT_ADDR, atoi((char *)value));
+    int readValue = EEPROM.read(TOO_HOT_ADDR);
+    Serial.print("Read value: ");
+    Serial.println(readValue);
+    // uint8_t readValue =
 }
 
 /*
